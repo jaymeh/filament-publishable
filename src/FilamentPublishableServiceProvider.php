039392323ffed304c505c181bf5530c3a709b0c0
jaymeh <jaymeh@users.noreply.github.com>
2023-08-24 13:21:26 +0000

    Fix styling

--- src/FilamentPublishableServiceProvider.php
+++ src/FilamentPublishableServiceProvider.php
@@ -9,12 +9,12 @@ use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
+use Jaymeh\FilamentPublishable\Commands\FilamentPublishableCommand;
+use Jaymeh\FilamentPublishable\Testing\TestsFilamentPublishable;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Jaymeh\FilamentPublishable\Commands\FilamentPublishableCommand;
-use Jaymeh\FilamentPublishable\Testing\TestsFilamentPublishable;
 
 class FilamentPublishableServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,10 +15,10 @@ use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Jaymeh\FilamentPublishable\FilamentPublishableServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Jaymeh\FilamentPublishable\FilamentPublishableServiceProvider;
 
 class TestCase extends Orchestra
 {
